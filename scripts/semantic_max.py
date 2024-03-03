e9485e3cde6cf5fd43ad8139573a194838dd2de3
riyasachdeva04 <riiya2048@gmail.com>
2024-03-04 00:52:42 +0530

    graph done

--- scripts/semantic_max.py
+++ scripts/semantic_max.py
@@ -2,24 +2,25 @@ import heapq
 import graph
 
 def dijkstra_longest_path(graph, src, dst):
-
-    distances = {node: 0 for node in graph}
-    queue = []
+   
+    distances = {node: 0 for node in graph}  
     visited = set()
+    paths = {src: [src]}
+    
+    queue = []
     heapq.heappush(queue, (0, src))
     
     while queue:
-        current_dist, current_node = heapq.heappop(queue)
-        
-        if current_node in visited:
-            continue
+       current_dist, current_node = heapq.heappop(queue)
+       if current_node in visited:
+           continue
             
-        visited.add(current_node) 
+       visited.add(current_node)
 
-        if current_node == dst:
-           return current_dist
+       if current_node == dst:
+           return current_dist, paths[dst]
            
-        for neighbor in graph[current_node]:
+       for neighbor in graph[current_node]:
            edge = graph[current_node][neighbor]
            weight = float(edge['weight'])
            
@@ -27,11 +28,16 @@ def dijkstra_longest_path(graph, src, dst):
            
            if distance > distances[neighbor]:
                distances[neighbor] = distance
+               paths[neighbor] = paths[current_node] + [neighbor] 
                heapq.heappush(queue, (distance, neighbor))
+               
+    return -float("inf"), []
 
-    return -float("inf")
+src = 'Bengali'
+dst = 'Marathi'
+# Print longest path    
+length, path = dijkstra_longest_path(graph.G, src, dst)
+print(length)
+print(path)
 
 
-src = 'English'
-dst = 'Urdu'
-print(dijkstra_longest_path(graph.G, src, dst))
